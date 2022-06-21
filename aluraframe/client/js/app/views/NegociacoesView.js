6602bf2fdca39065ae6cb004b8de742f4a554e90
Elton Marinho <elton.marinho97@gmail.com>
2022-06-21 10:02:31 -0300

    atualização do projeto

---
+++ aluraframe/client/js/app/views/NegociacoesView.js
@@ -0,0 +1,48 @@
+class NegociacoesView {
+
+    constructor(elemento) {
+
+        this._elemento = elemento;
+    }
+
+    _template(model) {
+        //dentro do tbody, é que vai ser criado um tr para adicionar uma nova negociação, é através da função map, que a gente consegue acessar onde estão as negociações
+
+        return `
+        <table class="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th>DATA</th>
+                        <th>QUANTIDADE</th>
+                        <th>VALOR</th> 
+                        <th>VOLUME</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    ${model.negociacoes.map (neg =>`
+                        <tr>
+                            <td>${DateHelper.dataParaTexto(neg.data)}</td>
+                            <td>${neg.quantidade}</td>
+                            <td>${neg.valor}</td>
+                            <td>${neg.volume}</td>
+                        </tr>
+                        `
+                    ).join('')}
+                </tbody>
+
+                <tfoot>
+                <td colspan="3"></td>
+                <td>${model.negociacoes.reduce((total,n) => total + n.volume, 0.0)}</td>
+                </tfoot>
+
+         </table>
+        `;
+    }
+
+    update(model) {
+
+        this._elemento.innerHTML = this._template(model);
+    }
+
+}
